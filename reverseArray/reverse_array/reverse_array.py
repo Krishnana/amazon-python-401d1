a7c6581e44231c2e38a23feddb519f42df60b352
Krishnan Ananthapadmanabhan <kaananth@amazon.com>
2020-04-20 15:57:25 -0700

    Reverse Array first commit

---
+++ reverseArray/reverse_array/reverse_array.py
@@ -0,0 +1,30 @@
+
+# Method to get user input
+def get_input():
+    input_list = []
+    try:
+        n = int(input("Enter number of elements"))
+        i = 1
+        while i<=n:
+            print(f"Enter number {i}")
+            input_list.append(int(input())) 
+            i = i + 1
+        print(f"Input list {input_list}") 
+    except: 
+        print("Please enter only integers") 
+    return input_list
+
+# Method to reverse an array
+def reverse_array(list):
+    count = len(list)
+    for i in range(count):
+        first_item = list[i]
+        if i < count/2:
+            j = count - i - 1
+            list[i] = list[j]
+            list[j] = first_item
+    print(f"Reverse Array is {list}")
+
+print("Provide an array to reverse")
+reverse_array(get_input())
+
